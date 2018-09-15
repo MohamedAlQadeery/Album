ea66181c1a3b93207da8f92eaf6c89066f66751d
Mohamed Al-Qadeery <moh9amad1@gmail.com>
2018-09-15 05:34:55 +0300

    album index

--- resources/views/album/index.blade.php
+++ resources/views/album/index.blade.php
@@ -4,22 +4,26 @@
 @section('content')
     <div class="jumbotron text-center">
         <h3>
-            @auth
-                Welcome {{\Illuminate\Support\Facades\Auth::user()->name}}
-            @endauth
-
+            @if(\Illuminate\Support\Facades\Auth::check() || \Illuminate\Support\Facades\Auth::guard('worker')->check())
+                @if(\Illuminate\Support\Facades\Auth::guard('web')->check())
+                    web
+                    <h3>Welcome {{\Illuminate\Support\Facades\Auth::guard('web')->user()}}</h3>
+                @else
+                    admin
+                    <h3>Welcome {{\Illuminate\Support\Facades\Auth::guard('worker')->user()}}</h3>
+                @endif
+            @endif
             @unless(\Illuminate\Support\Facades\Auth::check())
                 Welcome Guest .. to continue using our website you need to login
 
-            @endunless
         </h3>
 
-        @guest
+
             <div class="text-center">
             <p>Login to continue if you dont have account register</p>
             <a href="{{route('login')}}" class="btn btn-primary">Login</a>
             <a href="{{route('register')}}" class="btn btn-primary">Register</a>
             </div>
-        @endguest
+        @endunless
     </div>
 @endsection
